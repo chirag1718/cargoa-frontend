32658d096a806f89983a1f90c3db62582920f7c0
Chirag <chiragsonar1718@gmail.com>
2023-06-01 22:46:05 +0530

    Fix: added user state to retrieve userId to get all messages for manufacturer.

--- src/components/General/Dashboard/Cards.jsx
+++ src/components/General/Dashboard/Cards.jsx
@@ -1,15 +1,16 @@
 import { Card, CardContent } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import CargoaApi from "../../../apis/CargoaApi";
+import { useSelector } from "react-redux";
 const Cards = () => {
-  const { id } = useParams();
-  console.log(id);
+  const userData = useSelector((state) => state.auth);
+  const userId = userData.user._id;
+  console.log(userId);
   const [selectedItem, setSelectedItem] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await CargoaApi.get(`message/manufacturer/${id}`);
+        const response = await CargoaApi.get(`/message/manufacturer/${userId}`);
         setSelectedItem(response.data);
         console.log(response.data);
       } catch (err) {
@@ -23,7 +24,102 @@ const Cards = () => {
       {selectedItem &&
         selectedItem.map((item) => {
           return (
-            <div className="flex flex-col justify-center md:justify-start items-center md:flex-row my-4 gap-5">
+            <div className="flex flex-wrap justify-center md:justify-start items-center md:flex-row my-4 gap-5">
+              <div className="h-[200px]">
+                <Card
+                  className="w-[220px] h-[200px] rounded-lg"
+                  elevation={3}
+                  key={item.id}
+                >
+                  {/* Order details */}
+                  <div className="flex flex-col gap-5 p-3 h-[150px]">
+                    <span className="text-xs">Name:</span>
+                    <span className="text-xs">orderId: </span>
+                    <span className="text-xs">Transporter: </span>
+                    <span className="text-xs">Address:</span>
+                  </div>
+
+                  <CardContent className="border-t-2">
+                    <div className="font-semibold text-xs">₹ </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="h-[200px]">
+                <Card
+                  className="w-[220px] h-[200px] rounded-lg"
+                  elevation={3}
+                  key={item.id}
+                >
+                  {/* Order details */}
+                  <div className="flex flex-col gap-5 p-3 h-[150px]">
+                    <span className="text-xs">Name:</span>
+                    <span className="text-xs">orderId: </span>
+                    <span className="text-xs">Transporter: </span>
+                    <span className="text-xs">Address:</span>
+                  </div>
+
+                  <CardContent className="border-t-2">
+                    <div className="font-semibold text-xs">₹ </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="h-[200px]">
+                <Card
+                  className="w-[220px] h-[200px] rounded-lg"
+                  elevation={3}
+                  key={item.id}
+                >
+                  {/* Order details */}
+                  <div className="flex flex-col gap-5 p-3 h-[150px]">
+                    <span className="text-xs">Name:</span>
+                    <span className="text-xs">orderId: </span>
+                    <span className="text-xs">Transporter: </span>
+                    <span className="text-xs">Address:</span>
+                  </div>
+
+                  <CardContent className="border-t-2">
+                    <div className="font-semibold text-xs">₹ </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="h-[200px]">
+                <Card
+                  className="w-[220px] h-[200px] rounded-lg"
+                  elevation={3}
+                  key={item.id}
+                >
+                  {/* Order details */}
+                  <div className="flex flex-col gap-5 p-3 h-[150px]">
+                    <span className="text-xs">Name:</span>
+                    <span className="text-xs">orderId: </span>
+                    <span className="text-xs">Transporter: </span>
+                    <span className="text-xs">Address:</span>
+                  </div>
+
+                  <CardContent className="border-t-2">
+                    <div className="font-semibold text-xs">₹ </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="h-[200px]">
+                <Card
+                  className="w-[220px] h-[200px] rounded-lg"
+                  elevation={3}
+                  key={item.id}
+                >
+                  {/* Order details */}
+                  <div className="flex flex-col gap-5 p-3 h-[150px]">
+                    <span className="text-xs">Name:</span>
+                    <span className="text-xs">orderId: </span>
+                    <span className="text-xs">Transporter: </span>
+                    <span className="text-xs">Address:</span>
+                  </div>
+
+                  <CardContent className="border-t-2">
+                    <div className="font-semibold text-xs">₹ </div>
+                  </CardContent>
+                </Card>
+              </div>
               <div className="h-[200px]">
                 <Card
                   className="w-[220px] h-[200px] rounded-lg"
