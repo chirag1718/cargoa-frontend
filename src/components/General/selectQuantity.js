83417c31c2600889296b28450d9eab17ba3a2a09
Chirag <chiragsonar1718@gmail.com>
2023-06-02 17:27:59 +0530

    Fix: replaced number with string

--- src/components/General/selectQuantity.js
+++ src/components/General/selectQuantity.js
@@ -1,16 +1,16 @@
-const selectQuantity = [
+const SelectQuantity = [
   {
-    value: 1,
-    label: 1,
+    value: "1",
+    label: "1",
   },
   {
-    value: 2,
-    label: 2,
+    value: "2",
+    label: "2",
   },
   {
-    value: 3,
-    label: 3,
+    value: "3",
+    label: "3",
   },
 ];
 
-export default selectQuantity;
+export default SelectQuantity;
