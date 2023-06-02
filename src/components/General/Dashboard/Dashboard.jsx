dcc83415f6f3d6e0692d8e122c1bc20ae6119d86
Chirag <chiragsonar1718@gmail.com>
2023-06-02 08:35:32 +0530

    Fix: fixed width of the component

--- src/components/General/Dashboard/Dashboard.jsx
+++ src/components/General/Dashboard/Dashboard.jsx
@@ -16,10 +16,10 @@ const Dashboard = () => {
           Order Details
         </Typography>
       </div>
-      <div className="flex flex-col rounded-t-xl md:rounded-xl w-[250px] sm:w-[450px] md:w-[700px] lg:w-[900px] xl:w-[1300px] h-[450px] overflow-scroll scrollbar-hide border-2 md:px-5">
+      <div className="flex flex-wrap justify-center gap-5 md:gap-10 rounded-t-xl md:rounded-xl w-[250px] sm:w-[390px] md:w-[700px] lg:w-[900px] xl:w-[1300px] h-[450px] md:h-[500px] overflow-scroll scrollbar-hide border-2 pt-5">
         <Cards />
       </div>
-      <div className="flex justify-center items-center h-16 w-[250px] gap-2 border-b-2 border-x-2 rounded-b-xl md:hidden">
+      <div className="flex justify-center items-center h-16 w-[250px] sm:w-[450px] gap-2 border-b-2 border-x-2 rounded-b-xl md:hidden">
         <span className="text-lg transition-all ease-in duration-150 active:scale-95">
           Send Request
         </span>
