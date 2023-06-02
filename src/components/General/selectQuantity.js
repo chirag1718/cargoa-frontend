ae245556effbe4db67656fc55ddeb07036bc0f2d
Chirag <chiragsonar1718@gmail.com>
2023-06-02 10:33:58 +0530

    made an quantity array for message input form - quantity field

---
+++ src/components/General/selectQuantity.js
@@ -0,0 +1,16 @@
+const selectQuantity = [
+  {
+    value: 1,
+    label: 1,
+  },
+  {
+    value: 2,
+    label: 2,
+  },
+  {
+    value: 3,
+    label: 3,
+  },
+];
+
+export default selectQuantity;
