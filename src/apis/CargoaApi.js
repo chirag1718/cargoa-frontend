b6913b36ee5a34988b9d3f8dcf8a01c7da405315
Chirag <chiragsonar1718@gmail.com>
2023-06-03 17:45:17 +0530

    updated api

--- src/apis/CargoaApi.js
+++ src/apis/CargoaApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8800/api/v1/",
+  // baseURL: "http://localhost:8800/api/v1/",
+  baseURL: "https://busy-cyan-eagle.cyclic.app/api/v1/",
 });
